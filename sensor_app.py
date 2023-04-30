5b9b809cecdb351490a954b1e5f9fc2fadc7f42f
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-04-30 18:05:55 +1000

    added .ino file and update codee

--- sensor_app.py
+++ sensor_app.py
@@ -4,6 +4,10 @@ from random import random
 from threading import Lock
 from datetime import datetime
 import serial
+import pymysql
+import math
+import atexit
+import time
 
 """
 Background Thread
@@ -12,29 +16,65 @@ thread = None
 thread_lock = Lock()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'donsky!'
+app.config['SECRET_KEY'] = 'meow'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-ser = serial.Serial('/dev/ttyS0', 9600)
+threshold = 30
+
+# ser = serial.Serial('/dev/ttyS0', 9600)
+
+dbConn = pymysql.connect(host="localhost", user="pi", password="password", db="smoke_db")
+
+actuator_data = {
+    'buzzer': 0,
+    'fan': 0
+}
+
+timeout = 5
+
+prevTime = 0
 
 
 def get_current_datetime():
     now = datetime.now()
-    return now.strftime("%m/%d/%Y %H:%M:%S")
+    return now.strftime("%Y-%m-%d %H:%M:%S")
 
 """
 Get values and send it to our clients
 """
 def background_thread():
+    previous_value_id = None
     while True:
-        dummy_sensor_value = ser.readline()
-        socketio.emit('updateSensorData', {'value': dummy_sensor_value, "date": get_current_datetime()})
-        socketio.sleep(1)
+        sensor_value, push_button_value = get_value()
+
+        query = "INSERT INTO smoke_log (smoke, detected_datetime) VALUES ('%s', '%s')" % (sensor_value, get_current_datetime())
+        with dbConn.cursor() as cursor:
+            cursor.execute(query)
+        dbConn.commit()
+
+        select_query = "SELECT * FROM `smoke_log` ORDER BY detected_datetime DESC, smoke_id DESC LIMIT 1"
+        with dbConn.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute(select_query)
+            result = cursor.fetchone()
+
+            if time.time() - prevTime > timeout:
+                if result['smoke'] > threshold or push_button_value == 1:
+                    turn_buzzer_on()
+                    turn_fan_on()
+                else:
+                    turn_buzzer_off()
+                    turn_fan_off()
+                socketio.emit('updateActuatorData', actuator_data)
+
+            if (result['smoke_id'] != previous_value_id):
+                socketio.emit('updateSensorData', {'value': result['smoke'], "date": str(result['detected_datetime']), 'push_button': push_button_value})
+                previous_value_id = result['smoke_id']
+            socketio.sleep(0.2)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', threshold=threshold, actuators=actuator_data)
 
 
 @socketio.on('connect')
@@ -51,6 +91,59 @@ def connect():
 @socketio.on('disconnect')
 def disconnect():
     print('Client disconnected',  request.sid)
+
+@app.route('/<actuator>/<value>')
+def trigger_actuator(actuator, value):
+    global prevTime
+
+    prevTime = time.time()
+    if actuator == 'buzzer':
+        if value == 'on':
+            turn_buzzer_on()
+        elif value == 'off':
+            turn_buzzer_off()
+    elif actuator == 'fan':
+        if value == 'on':
+            turn_fan_on()
+        elif value == 'off':
+            turn_fan_off()
+
+    return render_template('index.html', threshold=threshold, actuators=actuator_data)
+
+@socketio.on('thresholdChange')
+def threshold_change(value):
+    print(value)
+    global threshold
+    threshold = int(value)
+
+def get_value():
+    # return ser.readline().decode('utf-8')
+    x = 1 if (random() > 0.5) else 0
+    return math.floor((random() * 50)), x
+def write_value(value):
+    # ser.write(value)
+    print("value: " + value.decode('utf-8'))
+
+@atexit.register
+def goodbye():
+    print("You are now leaving the Python file")
+    dbConn.close()
+
+def turn_buzzer_off():
+    actuator_data['buzzer'] = 0
+    write_value(b'1')
+
+def turn_buzzer_on():
+    actuator_data['buzzer'] = 1
+    write_value(b'2')
+
+def turn_fan_off():
+    actuator_data['fan'] = 0
+    write_value(b'3')
+
+def turn_fan_on():
+    actuator_data['fan'] = 1
+    write_value(b'4')
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,15 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", () => {
   const ctx = document.getElementById("myChart").getContext("2d");
 
   const myChart = new Chart(ctx, {
     type: "line",
     data: {
-      datasets: [{ label: "Temperature",  }],
+      datasets: [{ label: "Smoke",  }],
     },
     options: {
       borderWidth: 3,
       borderColor: ['rgba(255, 99, 132, 1)',],
+      animation: false
     },
   });
 
@@ -27,10 +28,10 @@ $(document).ready(function () {
     });
   }
 
-  const MAX_DATA_COUNT = 10;
+  const MAX_DATA_COUNT = 100;
   //connect to the socket server.
   //   var socket = io.connect("http://" + document.domain + ":" + location.port);
-  var socket = io.connect();
+  const socket = io.connect();
 
   //receive details from server
   socket.on("updateSensorData", function (msg) {
@@ -41,5 +42,19 @@ $(document).ready(function () {
       removeFirstData();
     }
     addData(msg.date, msg.value);
+
+    document.querySelector('#pushButton').textContent = msg.push_button
   });
+
+  socket.on('updateActuatorData', (msg) => {
+    document.querySelector('#buzzer').href = '/buzzer/' + msg.buzzer === 1 ? 'off' : 'on';
+    document.querySelector('#buzzerState').textContent = msg.buzzer === 1 ? 'on' : 'off';
+    document.querySelector('#fan').href = '/fan/' + msg.fan === 1 ? 'off' : 'on';
+    document.querySelector('#fanState').textContent = msg.fan === 1 ? 'on' : 'off';
+  })
+
+  document.querySelector("#threshold").addEventListener("input", (event) => {
+    socket.emit('thresholdChange', event.target.value)
+    document.querySelector("#thresholdValue").textContent = event.target.value;
+  })
 });
